5da2264bda323cfe0b42ece9980704c72068ea3d
MuhammadHaekal <muhammad.haekal@sprout.co.id>
2025-03-26 02:07:38 +0700

    [TICKET-1]: init repo

---
+++ src/common/configs/env.validation.ts
@@ -0,0 +1,61 @@
+import { plainToInstance } from 'class-transformer';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+  validateSync,
+} from 'class-validator';
+
+enum Environment {
+  Development = 'development',
+  Production = 'production',
+}
+
+export class EnvironmentVariables {
+  @IsEnum(Environment)
+  @IsNotEmpty()
+  NODE_ENV: Environment;
+
+  @IsNumber()
+  @Min(0)
+  @Max(65535)
+  @IsNotEmpty()
+  PORT: number;
+
+  @IsString()
+  @IsNotEmpty()
+  DB_HOST: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DB_PORT: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DB_USERNAME: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DB_PASSWORD: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DB_NAME: string;
+}
+
+export function envValidation(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+  return validatedConfig;
+}
